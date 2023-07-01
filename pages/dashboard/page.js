eef8974d8671b180543fd317300cf6222a3710e3
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-07-02 00:28:53 +0530

    refactor : components

--- pages/dashboard/page.js
+++ pages/dashboard/page.js
@@ -1,12 +1,20 @@
-import { Navigation } from '@/components/dashboard/TopNavigation'
-import { UserStats } from '@/components/dashboard/UserStats'
-import React from 'react'
+import { ProductActivities } from "@/components/dashboard/ProductActivities";
+import { ProductStat } from "@/components/dashboard/ProductStat";
+import { ProductStatsInfo } from "@/components/dashboard/ProductStatsInfo";
+import { Navigation } from "@/components/dashboard/TopNavigation";
+import React, { useEffect, useState } from "react";
 
-export default function Page(){
+export default function Page() {
+  const [domLoaded, setDomLoaded] = useState(false);
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
   return (
     <>
       <Navigation></Navigation>
-      <UserStats></UserStats>
+      <ProductStatsInfo></ProductStatsInfo>
+      <ProductActivities></ProductActivities>
+      <ProductStat></ProductStat>
     </>
   );
 }
