a63790395c9e3b23bf544974ce5f0dcb20fdba1f
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-07-02 01:01:38 +0530

    refactor : data

--- helpers/dummy.js
+++ helpers/dummy.js
@@ -1,53 +1,65 @@
-export const data ={
-    userStats : [
+export const data = {
+    userStats: [
       {
         iconUrl:
           "https://res.cloudinary.com/dqzjevzuo/image/upload/v1685772357/dzf9eded4n1fbvkyxgcx.png",
         statName: "Total Revenues",
         statValue: "$2,129,430",
-        bgColor: "bg-[#DDEFE0]",
-        dimensions:'h-[24px] w-[26px]'
+        bgColor: "#DDEFE0",
+        dimensions: {
+          height: 24,
+          width: 26,
+        },
       },
       {
         iconUrl:
           "https://res.cloudinary.com/dqzjevzuo/image/upload/v1685772357/qsjo2lvu6duko0kwyhn1.png",
         statName: "Total Transactions",
         statValue: "1,520",
-        bgColor: "bg-[#F4ECDD]",
-        dimensions:'h-[24px] w-[20px]'
+        bgColor: "#F4ECDD",
+        dimensions: {
+          height: 24,
+          width: 20,
+        },
       },
       {
         iconUrl:
           "https://res.cloudinary.com/dqzjevzuo/image/upload/v1685772357/vtyiizsirmhvsjikjiss.png",
         statName: "Total Likes",
         statValue: "9,721",
-        bgColor: "bg-[#EFDADA]",
-        dimensions:'w-[24px] h-[24px]'
+        bgColor: "#EFDADA",
+        dimensions: {
+          width: 24,
+          height: 24,
+        },
       },
       {
         iconUrl:
           "https://res.cloudinary.com/dqzjevzuo/image/upload/v1685772357/gaxvkedl59p1vczjhuhw.png",
         statName: "Total Users",
         statValue: "892",
-        bgColor: "bg-[#DEE0EF]",
-        dimensions:'h-[24px] w-[36px]'
+        bgColor: "#DEE0EF",
+        dimensions: {
+          height: 24,
+          width: 36,
+        },
       },
     ],
-     eventData : [
-    {
-      title: "Meeting with suppliers from Kuta Bali",
-      time: "14.00-15.00",
-      description: "at Sunset Road, Kuta, Bali ",
-      color: "border-[#9BDD7C]",
-    },
-    {
-      title: "Check operation at Giga Factory 1",
-      time: "18.00-20.00",
-      description: "at Central Jakarta ",
-      color: "border-[#6972C3]",
-    },
-  ],
-   navItems : [
+    eventData: [
+      {
+        title: "Meeting with suppliers from Kuta Bali",
+        time: "14.00-15.00",
+        description: "at Sunset Road, Kuta, Bali ",
+        color: "#9BDD7C",
+      },
+      {
+        title: "Check operation at Giga Factory 1",
+        time: "18.00-20.00",
+        description: "at Central Jakarta ",
+        color: "#6972C3",
+      },
+    ],
+    navItems: [
       {
         text: "Dashboard",
         iconUrl:
@@ -79,59 +91,61 @@ export const data ={
         isActive: false,
       },
     ],
-   lineChartData :[
-    {
-      name: "",
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: "Week 1",
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: "Week 2",
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: "Week 3",
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: "Week 4",
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: "",
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-  ],
-   pieChartData : [
-  {
-    name: "Group A",
-    value: 400,
-    fill: "#98D89E",
-  },
-  {
-    name: "Group B",
-    value: 300,
-    fill: "#EE8484",
-  },
-  {
-    name: "Group C",
-    value: 500,
-    fill: "#F6DC7D",
-  },
-]
-}
+    lineChartData: [
+      {
+        name: "",
+        uv: 400,
+        pv: 200,
+        amt: 400,
+      },
+      {
+        name: "Week 1",
+        uv: 300,
+        pv: 398,
+        amt: 220,
+      },
+      {
+        name: "Week 2",
+        uv: 200,
+        pv: 800,
+        amt: 290,
+      },
+      {
+        name: "Week 3",
+        uv: 270,
+        pv: 398,
+        amt: 200,
+      },
+      {
+        name: "Week 4",
+        uv: 180,
+        pv: 480,
+        amt: 211,
+      },
+      {
+        name: "",
+        uv: 180,
+        pv: 480,
+        amt: 211,
+      },
+    ],
+    pieChartData: [
+      {
+        name: "Basic Tees",
+        value: 55,
+        fill: "#98D89E",
+      },
+      {
+        name: "Custom Short Pants",
+        value: 14,
+        fill: "#F6DC7D",
+      },
+      
+      {
+        name: "Super Hoodies",
+        value: 31,
+        fill: "#EE8484",
+      },
+    ],
+  };
+  
