00c5ce865294efc8e13b6bf6344102db7f3db0ac
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-07-01 19:43:42 +0530

    feat : home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import { SignInForm } from "@/components/SignInForm";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,7 +13,18 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <main className="flex h-screen w-full">
+        <div className="h-full bg-[#000000] hidden  md:flex w-[40%] items-center justify-center">
+          <p className="text-[72px] text-white font-[700] leading-[88px]">
+            Board.{" "}
+          </p>
+        </div>
+        <div className="h-full flex flex-1 items-center justify-center bg-[#F5F5F5]">
+          <SignInForm />
+        </div>
+      </main>
       
     </>
-  )
+  );
 }
