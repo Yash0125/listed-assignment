c5e730b5b666d15bba363a3e3865e23b3323b534
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-07-02 14:35:50 +0530

    fix : nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,17 +1,11 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
-export const OPTIONS = {
-  session: { strategy: "jwt" },
-  providers: [
+export default NextAuth({
+  providers:[
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-  secret: process.env.SECRET,
-};
-
-const handler = NextAuth(OPTIONS);
-
-export { handler as GET, handler as POST };
+})
